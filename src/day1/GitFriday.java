e242cde7d17e016aa5b075e89cfcc599bfdff7bc
Tuncay72 <t.baytak072@gmail.com>
2020-04-24 13:42:24 -0500

    add GitFriday.java

---
+++ src/day1/GitFriday.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class GitFriday {
+    public static void main(String[] args) {
+        System.out.println("Happy to see you");
+    }
+}
--- src/day1/HelloGitWorld.java
+++ src/day1/HelloGitWorld.java
@@ -2,6 +2,6 @@ package day1;
 
 public class HelloGitWorld {
     public static void main(String[] args) {
-        System.out.println("Hwllo World");
+        System.out.println("Hello World");
     }
 }
