f955284571aa5537858364a12d599ce0c1efe37d
Tuncay72 <t.baytak072@gmail.com>
2020-04-24 15:34:56 -0500

    add login.java

--- src/day1/GitFriday.java
+++
@@ -1,7 +0,0 @@
-package day1;
-
-public class GitFriday {
-    public static void main(String[] args) {
-        System.out.println("Happy to see you");
-    }
-}
---
+++ src/day1/Login.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Login {
+    public static void main(String[] args) {
+        // Login...
+
+    }
+}
